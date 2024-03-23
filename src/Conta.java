fc217a55fdf5631bb7bf5908904f9dd46075316a
Bruno Bandeira <113468374+Bruno-BandeiraH@users.noreply.github.com>
2024-03-22 22:07:13 -0300

    Update Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -1,52 +1,52 @@
+public abstract class Conta implements IConta{
 
-public abstract class Conta implements IConta {
-	
-	private static final int AGENCIA_PADRAO = 1;
-	private static int SEQUENCIAL = 1;
+    private static int SEQUENCIAL = 1;
+    private static final int AGENCIA_PADRAO = 1;
 
-	protected int agencia;
-	protected int numero;
-	protected double saldo;
-	protected Cliente cliente;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
 
-	public Conta(Cliente cliente) {
-		this.agencia = Conta.AGENCIA_PADRAO;
-		this.numero = SEQUENCIAL++;
-		this.cliente = cliente;
-	}
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
 
-	@Override
-	public void sacar(double valor) {
-		saldo -= valor;
-	}
+    // GETTERS AND SETTERS
+    public int getAgencia() {
+        return agencia;
+    }
 
-	@Override
-	public void depositar(double valor) {
-		saldo += valor;
-	}
+    public int getNumero() {
+        return numero;
+    }
 
-	@Override
-	public void transferir(double valor, IConta contaDestino) {
-		this.sacar(valor);
-		contaDestino.depositar(valor);
-	}
+    public double getSaldo() {
+        return saldo;
+    }
 
-	public int getAgencia() {
-		return agencia;
-	}
+    // METHODS
+    @Override
+    public void sacar(Double valor) {
+        this.saldo -= valor;
+    }
 
-	public int getNumero() {
-		return numero;
-	}
+    @Override
+    public void depositar(Double valor) {
+        saldo += valor;
+    }
 
-	public double getSaldo() {
-		return saldo;
-	}
+    @Override
+    public void transferir(Double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
 
-	protected void imprimirInfosComuns() {
-		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
-		System.out.println(String.format("Agencia: %d", this.agencia));
-		System.out.println(String.format("Numero: %d", this.numero));
-		System.out.println(String.format("Saldo: %.2f", this.saldo));
-	}
+    @Override
+    public void imprimirSaldo() {
+        System.out.println(String.format("seu saldo é: %.2f", this.saldo));
+    }
+   
 }
