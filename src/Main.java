06ac907d4f9f269242d3ece4e8fe656057132dea
Bruno Bandeira <113468374+Bruno-BandeiraH@users.noreply.github.com>
2024-03-22 22:08:47 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,20 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
-
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+		Cliente bruno = new Cliente("Bruno");
+	        Cliente henrique = new Cliente("Henrique");
+	
+	        Conta contaCorrente = new ContaCorrente(bruno);
+	        contaCorrente.depositar(350.0);
+	
+	        Conta poupanca = new ContaPoupanca(henrique);
+	
+	        contaCorrente.transferir(100.0, poupanca);
+	
+	        contaCorrente.imprimirSaldo();
+	        poupanca.imprimirSaldo();
+	        System.out.println(bruno.getNome());
 	}
 
 }
